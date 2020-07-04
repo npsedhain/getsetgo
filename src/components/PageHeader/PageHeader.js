00990cd076c5eced4538a47355a92282ee6f8a90
npsedhain <npsedhain@gmail.com>
2020-07-04 14:46:23 +0545

    add simple page header component for app intro.

---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,22 @@
+/**
+ * The main product page.
+ */
+import React from 'react';
+
+class PageHeader extends React.Component {
+  render() {
+    return (
+      <div className='page-header header-filter'>
+        <div className='squares square1' />
+        <div className='squares square2' />
+        <div className='squares square3' />
+        <div className='squares square4' />
+        <div className='squares square5' />
+        <div className='squares square6' />
+        <div className='squares square7' />
+      </div>
+    );
+  }
+}
+
+export default PageHeader;
